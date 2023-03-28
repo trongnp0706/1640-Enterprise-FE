d97fb5c926e545444d6ea7244ad82737ee0c395f
Huu Tung <tungvnhgcd191114@fpt.edu.vn>
2023-03-28 20:25:11 +0700

    Create Post Card

--- 1640-frontend/src/components/createPost/CreatePost.jsx
+++ 1640-frontend/src/components/createPost/CreatePost.jsx
@@ -4,6 +4,10 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import SendIcon from "@mui/icons-material/Send";
+import React from "react";
+import Switch from "react-switch";
 const CreatePost = () => {
     const { currentUser } = useContext(AuthContext);
     const userId = currentUser?.data?.user?.id;
@@ -75,16 +79,28 @@ const CreatePost = () => {
                             onChange={(e) => setTitle(e.target.value)}
                             value={title}
                         />
-                        <input
-                            type="text"
-                            placeholder={`What's on your mind ${currentUser?.data?.user?.username}?`}
-                            onChange={(e) => setContent(e.target.value)}
-                            value={content}
-                        />
                     </div>
+                </div>
+                <div className="mid-content">
+                    <input
+                        type="text"
+                        placeholder={`What's on your mind ${currentUser?.data?.user?.username}?`}
+                        onChange={(e) => setContent(e.target.value)}
+                        value={content}
+                    />
+                </div>
+                <div className="mid-image">
                     <div className="right">
                         {files.map((file, index) => (
                             <img key={index} className="file" alt="" src={URL.createObjectURL(file)} />
+                        ))}
+                        {files.map((file, index) => (
+                            <HighlightOffIcon
+                                id="deleteIcon"
+                                onClick={() => {
+                                    document.getElementById("file").remove();
+                                }}
+                            />
                         ))}
                     </div>
                 </div>
@@ -107,9 +123,33 @@ const CreatePost = () => {
                                 <span>Add Image</span>
                             </div>
                         </label>
+                        <div>
+                            <Switch
+                                // checked={this.state.checked}
+                                // onChange={this.handleChange}
+                            />
+                        </div>
+                        <div>
+                            <p>Anonymous</p>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="category-select">Category:</label>
+                        <select id="category-select">
+                            <option value="cat">CAT</option>
+                            <option value="dog">DOG</option>
+                        </select>
+                        <label htmlFor="academic-year-select">Academic Year:</label>
+                        <select id="academic-year-select">
+                            <option value="2022">2022</option>
+                            <option value="2023">2023</option>
+                        </select>
                     </div>
                     <div className="right">
-                        <button onClick={handleClick}>Share</button>
+                        <SendIcon
+                            onClick={handleClick}
+                            style={{ color: "blue" }}
+                        ></SendIcon>
                     </div>
                 </div>
             </div>
