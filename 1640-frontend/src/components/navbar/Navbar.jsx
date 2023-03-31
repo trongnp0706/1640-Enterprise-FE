c818e56034db64a10b7e664123c05b48a6e7bb71
Thinh <nguyenhungthinh2504@gmail.com>
2023-03-31 16:58:12 +0700

    fix navbar responsive & edit navbar

--- 1640-frontend/src/components/navbar/Navbar.jsx
+++ 1640-frontend/src/components/navbar/Navbar.jsx
@@ -1,61 +1,52 @@
-import "./navbar.scss";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
-import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { Link, useNavigate } from "react-router-dom";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
 import { useContext } from "react";
-import { DarkModeContext } from "../../context/darkModeContext";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
+import { DarkModeContext } from "../../context/darkModeContext";
+import "./navbar.scss";
 
 const Navbar = () => {
-    const { toggle, darkMode } = useContext(DarkModeContext);
-    const { currentUser, logout } = useContext(AuthContext);
-    const userId = currentUser?.data?.user?.id;
-    console.log(currentUser)
+  const { toggle, darkMode } = useContext(DarkModeContext);
+  const { currentUser, logout } = useContext(AuthContext);
+  const userId = currentUser?.data?.user?.id;
+  console.log(currentUser);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = async (e) => {
-        e.preventDefault();
+  const handleClick = async (e) => {
+    e.preventDefault();
 
-        try {
-            await logout();
-            navigate("/login");
-        } catch (err) {
-            console.log("Logout failed with error:" + err);
-        }
-    };
+    try {
+      await logout();
+      navigate("/login");
+    } catch (err) {
+      console.log("Logout failed with error:" + err);
+    }
+  };
 
-    return (
-        <div className="navbar">
-            <div className="left">
-                <Link to="/" style={{ textDecoration: "none" }}>
-                    <span>1640 Web Enterprise</span>
-                </Link>
-                {darkMode ? (
-                    <WbSunnyOutlinedIcon onClick={toggle} />
-                ) : (
-                    <DarkModeOutlinedIcon onClick={toggle} />
-                )}
-                <div className="search">
-                    <SearchOutlinedIcon />
-                    <input type="text" placeholder="Search..." />
-                </div>
-            </div>
-            <div className="right">
-                <Link to={`/profile/${userId}`} className="user">
-                    <img src={currentUser?.data?.user?.avatar} alt="" />
-                </Link>
-                <LogoutIcon onClick={handleClick} />
-            </div>
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>1640 Web Enterprise</span>
+        </Link>
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
         </div>
-    );
+      </div>
+
+      <div className="right">
+        <Link to={`/profile/${userId}`} className="user">
+          <img src={currentUser?.data?.user?.avatar} alt="" />
+        </Link>
+        <LogoutIcon onClick={handleClick} />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
