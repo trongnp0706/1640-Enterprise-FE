2b0da221ce2eff497e05c7dfbfdd7cab4dd318ac
Thinh <nguyenhungthinh2504@gmail.com>
2023-04-15 15:36:06 +0700

    edit leftBar

--- 1640-frontend/src/components/leftBar/LeftBar.jsx
+++ 1640-frontend/src/components/leftBar/LeftBar.jsx
@@ -1,72 +1,116 @@
-import "./leftBar.scss";
+import { useQueryClient } from "@tanstack/react-query";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import Image from "../../assets/img.png";
 import { AuthContext } from "../../context/authContext";
-import {useContext, useState} from "react";
-import {Link} from "react-router-dom";
-import {PageContext} from "../../context/pageContext";
-import {useQueryClient} from "@tanstack/react-query";
+import { PageContext } from "../../context/pageContext";
+import "./leftBar.scss";
 
 const LeftBar = () => {
+  const { currentUser } = useContext(AuthContext);
+  const { currentPage, setCurrentPage } = useContext(PageContext);
+  const queryClient = useQueryClient();
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
-    const { currentUser } = useContext(AuthContext);
-    const { currentPage, setCurrentPage } = useContext(PageContext);
-    const queryClient = useQueryClient();
-    const handleNextPage = () => {
-        setCurrentPage(currentPage + 1);
-    };
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
 
-    const handlePreviousPage = () => {
-        setCurrentPage(currentPage - 1);
-    };
-
-    return (
-        <div className="leftBar">
-            <div className="container">
-                <div className="menu">
-                    <div className="user">
-                        <img
-                            src={currentUser?.data?.user?.avatar}
-                            alt=""
-                        />
-                        <span>{currentUser?.data?.user?.username}</span>
-                    </div>
-                    <hr />
-                    <Link to={`/`}>
-                        <div className="item">
-                            <span>Latest Posts</span>
-                        </div>
-                    </Link>
-                    <Link to={`/popular`}>
-                    <div className="item">
-                        <span>Most Popular Posts</span>
-                    </div>
-                    </Link>
-                    <Link to={`/view`}>
-                    <div className="item">
-                        <span>Most Viewed Post</span>
-                    </div>
-                    </Link>
-                    <div className="item">
-                        <span>Statistics</span>
-                    </div>
-                    <hr />
-                        <div>
-                            <button
-                                onClick={handlePreviousPage}
-                                disabled={currentPage === 1}
-                            >
-                                Prev
-                            </button>
-                            <span> Page {currentPage} </span>
-                            <button
-                                onClick={handleNextPage}
-                            >
-                                Next
-                            </button>
-                        </div>
-                </div>
+  return (
+    <div className="leftBar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img src={currentUser?.data?.user?.avatar} alt="" />
+            <span>{currentUser?.data?.user?.username}</span>
+          </div>
+          <hr />
+          <Link
+            to={`/`}
+            style={{
+              textDecoration: "none",
+              color: "black",
+              fontWeight: "bold",
+            }}
+          >
+            <div className="item">
+              <span>Latest Posts</span>
             </div>
+            <div class="image-and-title">
+              <div className="image">
+                <img src={Image} alt=""></img>
+              </div>
+              <div className="title">
+                <span>title</span>
+                <p>49 votes, 28 comments, Mar 26</p>
+              </div>
+            </div>
+          </Link>
+          <Link
+            to={`/popular`}
+            style={{
+              textDecoration: "none",
+              color: "black",
+              fontWeight: "bold",
+            }}
+          >
+            <div className="item">
+              <span>Most Popular Posts</span>
+            </div>
+            <div class="image-and-title">
+              <div className="image">
+                <img src={Image} alt=""></img>
+              </div>
+              <div className="title">
+                <span>title</span>
+                <p>49 votes, 28 comments, Mar 26</p>
+              </div>
+            </div>
+          </Link>
+          <Link
+            to={`/view`}
+            style={{
+              textDecoration: "none",
+              color: "black",
+              fontWeight: "bold",
+            }}
+          >
+            <div className="item">
+              <span>Most Viewed Post</span>
+            </div>
+            <div class="image-and-title">
+              <div className="image">
+                <img src={Image} alt=""></img>
+              </div>
+              <div className="title">
+                <span>title</span>
+                <p>49 votes, 28 comments, Mar 26</p>
+              </div>
+            </div>
+          </Link>
+          <div className="item">
+            <span>Statistics</span>
+          </div>
+          <hr />
+          <div className="pagination">
+            <button
+              className="pagination-button"
+              onClick={handlePreviousPage}
+              disabled={currentPage === 1}
+            >
+              &lt; Prev
+            </button>
+            <span className="pagination-current-page"> {currentPage}</span>
+            <button className="pagination-button" onClick={handleNextPage}>
+              Next &gt;
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LeftBar;
--- 1640-frontend/src/pages/year/YearPage.jsx
+++ 1640-frontend/src/pages/year/YearPage.jsx
@@ -1,61 +1,60 @@
 import Menu from "@mui/material/Menu";
-import {useMutation, useQueryClient} from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
 import { Link } from "react-router-dom";
+import { makeRequest } from "../../axios";
 import Years from "../../components/years/Years";
 import "./yearPage.scss";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import {makeRequest} from "../../axios";
 
 const YearPage = () => {
   const queryClient = useQueryClient();
-    const [year, setYear] = useState("");
-    const [closure_date, setClosureDate] = useState("");
-    const [date, setDate] = useState(new Date());
+  const [year, setYear] = useState("");
+  const [closure_date, setClosureDate] = useState("");
+  const [date, setDate] = useState(new Date());
   const [showMenuAdd, setShowMenuAdd] = useState(false);
   const [addAnchorEl, setAddAnchorEl] = useState(null);
 
-    const handleDateChange = (date) => {
-        setDate(date);
-        const parsedDate = date;
+  const handleDateChange = (date) => {
+    setDate(date);
+    const parsedDate = date;
 
-        const year = parsedDate.getFullYear();
-        const month = String(parsedDate.getMonth() + 1).padStart(2, '0');
-        const day = String(parsedDate.getDate()).padStart(2, '0');
-        const hours = String(parsedDate.getHours()).padStart(2, '0');
-        const minutes = String(parsedDate.getMinutes()).padStart(2, '0');
-        const seconds = String(parsedDate.getSeconds()).padStart(2, '0');
+    const year = parsedDate.getFullYear();
+    const month = String(parsedDate.getMonth() + 1).padStart(2, "0");
+    const day = String(parsedDate.getDate()).padStart(2, "0");
+    const hours = String(parsedDate.getHours()).padStart(2, "0");
+    const minutes = String(parsedDate.getMinutes()).padStart(2, "0");
+    const seconds = String(parsedDate.getSeconds()).padStart(2, "0");
 
-        const isoDateString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+    const isoDateString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
 
-        setClosureDate(isoDateString);
-    };
+    setClosureDate(isoDateString);
+  };
 
-    const mutation = useMutation(
-        (newYear) => {
-            return makeRequest.post("year/add", newYear);
-        },
-        {
-            onSuccess: () => {
-                // Invalidate and refetch
-                queryClient.invalidateQueries(["years"]);
-            },
-        }
-    );
+  const mutation = useMutation(
+    (newYear) => {
+      return makeRequest.post("year/add", newYear);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["years"]);
+      },
+    }
+  );
 
-    const handleAddClick = (event) => {
+  const handleAddClick = (event) => {
     setAddAnchorEl(event.currentTarget);
     setShowMenuAdd(true);
   };
 
-
   const handleAddClose = () => {
     setAddAnchorEl(null);
     setShowMenuAdd(false);
-      mutation.mutate({academic_year: year, closure_date: closure_date})
-      setYear("");
-      setDate("");
+    mutation.mutate({ academic_year: year, closure_date: closure_date });
+    setYear("");
+    setDate("");
   };
 
   const handleAddCloseMenu = () => {
@@ -113,15 +112,13 @@ const YearPage = () => {
           }}
         >
           <div>
-            <label style={{ fontWeight: "bold", margin: "10px" }}>
-              Year
-            </label>
+            <label style={{ fontWeight: "bold", margin: "10px" }}>Year</label>
             <input
               type="text"
               onChange={(e) => setYear(e.target.value)}
               value={year}
               style={{
-                width: "85%",
+                width: "80%",
                 height: "70%",
                 borderRadius: "10px",
                 margin: "10px",
@@ -129,21 +126,19 @@ const YearPage = () => {
               }}
             />
           </div>
-
-          <div>
+          <div style={{ width: "380px", height: "500px" }}>
             <label style={{ fontWeight: "bold", margin: "10px" }}>
               Closure Date
             </label>
-              <DatePicker
-                  selected={date}
-                  onChange={handleDateChange}
-                  showTimeSelect
-                  timeFormat="HH:mm"
-                  timeIntervals={15}
-                  dateFormat="yyyy-MM-dd HH:mm"
-              />
+            <DatePicker
+              selected={date}
+              onChange={handleDateChange}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="yyyy-MM-dd HH:mm"
+            />
           </div>
-
           <div style={{ height: "40px" }}>
             <button
               style={{
@@ -153,7 +148,7 @@ const YearPage = () => {
                 backgroundColor: "black",
                 borderRadius: "10px",
                 color: "white",
-                marginLeft: "27%",
+                marginLeft: "25%",
               }}
               onClick={handleAddClose}
             >
